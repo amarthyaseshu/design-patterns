4ecb96a9017047da479919e4e1af5e5aa2e0867f
amarthya <amarthyaseshu2@gmail.com>
2025-04-29 23:16:30 +0530

    commit

--- src/builder/Employee.java
+++ src/builder/Employee.java
@@ -15,8 +15,13 @@ public class Employee {
         private String name;
         private String empId;
 
+
         public EmployeeBuilder buildName(String name){
             this.name=name;
+            //  In Java, a static nested class can use this keyword within its instance methods to
+            //  refer to its own instance. However, it does not have access to an enclosing instance of the
+            //  outer class (since it is static), so OuterClass.this is invalid. Here's a breakdown:
+            //  this is not allowed in static methods of the static nested class.
             return this;
         }
 
--- src/factory/FactoryMain.java
+++ src/factory/FactoryMain.java
@@ -2,8 +2,6 @@ package factory;
 
 public class FactoryMain {
 
-
-
     public static void main(String[] args) {
         SaveWorld saveWorld=new SaveWorld();
         Avengers easyAvenger = saveWorld.getAvenger("easy");
